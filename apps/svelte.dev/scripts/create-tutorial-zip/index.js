a3292e4c98e5224077e58cfd617e28e9d0274200
Rich Harris <rich.harris@vercel.com>
2024-06-12 15:07:15 -0400

    fix

--- apps/svelte.dev/scripts/create-tutorial-zip/index.js
+++ apps/svelte.dev/scripts/create-tutorial-zip/index.js
@@ -29,10 +29,7 @@ if (
 		'node_modules/rollup/dist/shared'
 	];
 
-	const ignored_files = new Set([
-		'node_modules/svelte/compiler.cjs',
-		'node_modules/svelte/compiler/index.js'
-	]);
+	const ignored_files = new Set(['node_modules/svelte/compiler/index.js']);
 
 	for (const file of glob('**', { cwd, filesOnly: true, dot: true }).map((file) =>
 		file.replaceAll('\\', '/')
