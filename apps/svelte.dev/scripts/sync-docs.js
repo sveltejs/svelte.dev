92a6b9e8984923dcab65efb199c28bdf873582eb
Simon Holthausen <simon.holthausen@vercel.com>
2024-06-17 18:24:50 +0200

    bring over one svelte reference page

--- apps/svelte.dev/scripts/sync-docs.js
+++ apps/svelte.dev/scripts/sync-docs.js
@@ -5,6 +5,39 @@ import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 import { stringify_module } from '@sveltejs/site-kit/markdown';
 
+// Adjust these as needed for your local setup
+const svelte_path = '../../../../svelte/sites/svelte.dev';
+const sveltekit_path = '../../../../svelte-kit/sites/kit.svelte.dev';
+
+// Run script to generate type information from Svelte, then load the resulting JS file.
+// Once everything's on svelte.dev simplify this process by using the generated JSON directly.
+const svelte_script_path = fileURLToPath(
+	new URL(`${svelte_path}/scripts/type-gen/index.js`, import.meta.url).href
+);
+const svelte_json_path = new URL(`${svelte_path}/src/lib/generated/type-info.js`, import.meta.url)
+	.href;
+execSync(`node ${svelte_script_path}`, {
+	cwd: path.dirname(path.dirname(path.dirname(svelte_script_path)))
+});
+const { modules: svelte_modules } = await import(svelte_json_path);
+write_module_to_md(svelte_modules, 'svelte');
+
+// Same for SvelteKit
+const sveltekit_script_path = fileURLToPath(
+	new URL(`${sveltekit_path}/scripts/types/index.js`, import.meta.url).href
+);
+const sveltekit_json_path = new URL(
+	`${sveltekit_path}/src/lib/generated/type-info.js`,
+	import.meta.url
+).href;
+execSync(`node ${sveltekit_script_path}`, {
+	cwd: path.dirname(path.dirname(path.dirname(sveltekit_script_path)))
+});
+const { modules: sveltekit_modules } = await import(sveltekit_json_path);
+write_module_to_md(sveltekit_modules, 'kit');
+
+// Helper methods
+
 /** @param {string} dir */
 function mkdirp(dir) {
 	try {
@@ -18,31 +51,6 @@ function mkdirp(dir) {
 		}
 		throw e;
 	}
-}
-
-function module_to_string(module, depth = 1) {
-	let content = `#${'#'.repeat(depth)} ${module.name}\n\n`;
-
-	if (module.snippet) {
-		content += `\`\`\`typescript\n${module.snippet}\n\`\`\`\n\n`;
-	}
-
-	if (module.comment) {
-		content += `${module.comment}\n\n`;
-	}
-
-	if (module.bullets?.length) {
-		content += '> Bullets\n\n';
-		for (const bullet of module.bullets) {
-			content += `${bullet}\n`;
-		}
-		content += '\n';
-	}
-
-	for (const child of module.children) {
-		content += module_to_string(child, depth + 1);
-	}
-	return content;
 }
 
 function write_module_to_md(modules, name) {
@@ -60,15 +68,8 @@ function write_module_to_md(modules, name) {
 	}
 
 	// for each module, generate the markdown content and replace the include in the file
-	// TODO support more granular includes
+	// TODO support more granular includes?
 	for (const module of modules) {
-		// let generated = `${module.comment}\n\n`;
-		// for (const _export of module.exports) {
-		// 	generated += module_to_string(_export);
-		// }
-		// for (const type of module.types) {
-		// 	generated += module_to_string(type);
-		// }
 		const generated = stringify_module(module);
 
 		for (const [name, content] of Object.entries(fileContents)) {
@@ -95,19 +96,3 @@ function write_module_to_md(modules, name) {
 		writeFileSync(path.join(dir, name), content);
 	}
 }
-
-// Run script to generate type information from SvelteKit, then load the resulting JS file.
-// Once everything's on svelte.dev simplify this process by using the generated JSON directly.
-const sveltekit_script_path = fileURLToPath(
-	new URL('../../../../svelte-kit/sites/kit.svelte.dev/scripts/types/index.js', import.meta.url)
-		.href
-);
-const sveltekit_json_path = new URL(
-	'../../../../svelte-kit/sites/kit.svelte.dev/src/lib/generated/type-info.js',
-	import.meta.url
-).href;
-execSync(`node ${sveltekit_script_path}`, {
-	cwd: path.dirname(path.dirname(path.dirname(sveltekit_script_path)))
-});
-const { modules: sveltekit_modules } = await import(sveltekit_json_path);
-write_module_to_md(sveltekit_modules, 'kit');
