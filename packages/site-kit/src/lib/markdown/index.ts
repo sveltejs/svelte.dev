375efb7eefc462215c3bfc8fccb63f0f7af8b219
Rich Harris <rich.harris@vercel.com>
2024-06-19 14:41:36 -0400

    more

---
+++ packages/site-kit/src/lib/markdown/index.ts
@@ -0,0 +1,33 @@
+export {
+	render_content_markdown as renderContentMarkdown,
+	replace_export_type_placeholders as replaceExportTypePlaceholders,
+	stringify_module,
+	stringify_type,
+	stringify_expanded_type
+} from './renderer';
+
+export {
+	extract_frontmatter as extractFrontmatter,
+	transform as markedTransform,
+	normalizeSlugify,
+	slugify,
+	removeMarkdown,
+	escape
+} from './utils';
+
+export interface Modules {
+	name?: string;
+	comment?: string;
+	exempt?: boolean;
+	types?: ModuleChild[];
+	exports?: ModuleChild[];
+}
+
+export interface ModuleChild {
+	name: string;
+	snippet: string;
+	comment: string;
+	deprecated?: string;
+	bullets?: string[];
+	children?: ModuleChild[];
+}
