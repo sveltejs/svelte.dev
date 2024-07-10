7196c3d0101fb2725b22ab910deff77579565790
Ben McCann <322311+benmccann@users.noreply.github.com>
2024-07-10 12:43:04 -0700

    cleanup

--- apps/svelte.dev/scripts/sync-repos.js
+++ apps/svelte.dev/scripts/sync-repos.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 import { spawn } from 'node:child_process';
-import { existsSync } from 'node:fs';
+import { existsSync, readFileSync } from 'node:fs';
 
 let cwd = process.cwd();
 while ((cwd.match(/svelte.dev/g) || []).length > 0) {
@@ -12,21 +12,36 @@ while ((cwd.match(/svelte.dev/g) || []).length > 0) {
 cloneRepo('git@github.com:sveltejs/svelte.git');
 cloneRepo('git@github.com:sveltejs/kit.git');
 
+/**
+ * @param {string} repo
+ */
 function cloneRepo(repo) {
-	const dirname = /git@github.com:\w+\/(\w+).git/.exec(repo)[1];
+	const regex_result = /git@github.com:\w+\/(\w+).git/.exec(repo);
+	if (!regex_result || regex_result.length < 2) {
+		throw new Error(`Expected git@github.com:xxx/xxx.git, but got ${repo}`);
+	}
+	const dirname = regex_result[1];
 	if (existsSync(dirname)) {
 		return console.log(`${dirname} exists. skipping git clone`);
 	}
 	invoke('git', ['clone', '--depth', '1', repo]);
 }
 
+/**
+ * @param {string} cmd
+ * @param {string[]} args
+ * @return {Promise<void>}
+ */
 function invoke(cmd, args) {
 	const child = spawn(cmd, args);
 	child.stdout.on('data', (data) => console.log(data.toString()));
 	child.stderr.on('data', (data) => console.error(data.toString()));
-	child.on('close', (code) => {
-		if (!code) {
-			console.log(`${[cmd, ...args].join(' ')} successfully completed`);
-		}
+	return new Promise((resolve) => {
+		child.on('close', (code) => {
+			if (!code) {
+				console.log(`${[cmd, ...args].join(' ')} successfully completed`);
+			}
+			resolve();
+		});
 	});
 }
