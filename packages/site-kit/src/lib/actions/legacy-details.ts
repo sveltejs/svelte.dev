109e6acb7b82dc46fa1c5d94472222696b012b3f
Rich Harris <rich.harris@vercel.com>
2024-09-24 14:29:56 -0400

    fix position when toggling other elements

--- packages/site-kit/src/lib/actions/legacy-details.ts
+++ packages/site-kit/src/lib/actions/legacy-details.ts
@@ -8,21 +8,49 @@ export function legacy_details(node: HTMLElement) {
 	let details: NodeListOf<HTMLDetailsElement>;
 
 	function update() {
-		details = node.querySelectorAll('details.legacy');
+		/** @type {HTMLElement | null} */
+		let scroll_parent = node;
+
+		while ((scroll_parent = scroll_parent.parentElement)) {
+			if (/^(scroll|auto)$/.test(getComputedStyle(scroll_parent).overflowY)) {
+				break;
+			}
+		}
+
+		details = /** @type {NodeListOf<HTMLDetailsElement>} */ node.querySelectorAll('details.legacy');
 		const show = get(show_legacy);
+
+		/** Whether the toggle was initiated by user action or `element.open = !element.open` */
+		let secondary = false;
 
 		// Add a select to each code block
 		for (const detail of details) {
 			detail.open = show;
 
-			detail.addEventListener('toggle', () => {
+			detail.addEventListener('toggle', (e) => {
+				if (secondary) return;
+				secondary = true;
+
 				show_legacy.set(detail.open);
+
+				const top = detail.getBoundingClientRect().top;
 
 				for (const other of details) {
 					if (other !== detail) {
 						other.open = detail.open;
 					}
 				}
+
+				const delta = detail.getBoundingClientRect().top - top;
+
+				if (delta !== 0) {
+					// whichever element the user interacted with should stay in the same position
+					scroll_parent.scrollBy(0, delta);
+				}
+
+				setTimeout(() => {
+					secondary = false;
+				});
 			});
 		}
 	}
