cc38f11afeca5f92423acf20959f64e3aab9a0c3
Rich Harris <rich.harris@vercel.com>
2024-09-30 20:48:03 -0400

    remove some unused code

--- apps/svelte.dev/src/routes/blog/[slug]/+page.svelte
+++ apps/svelte.dev/src/routes/blog/[slug]/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import { page } from '$app/stores';
-	import { copy_code_descendants, ts_js_select } from '@sveltejs/site-kit/actions';
+	import { copy_code_descendants } from '@sveltejs/site-kit/actions';
 	import { Text } from '@sveltejs/site-kit/components';
 	import { setupDocsHovers } from '@sveltejs/site-kit/docs';
 
@@ -22,7 +22,7 @@
 </svelte:head>
 
 <div class="content">
-	<article class="post listify text" use:ts_js_select use:copy_code_descendants>
+	<article class="post listify text" use:copy_code_descendants>
 		<h1>{data.metadata.title}</h1>
 		<p class="standfirst">{data.metadata.description}</p>
 
