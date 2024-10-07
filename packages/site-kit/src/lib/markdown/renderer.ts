3d2b6ddc620c85e9054d6f63a5136f3aad1941b4
Rich Harris <rich.harris@vercel.com>
2024-10-07 13:36:50 -0400

    use borders instead of box-shadow

--- packages/site-kit/src/lib/markdown/renderer.ts
+++ packages/site-kit/src/lib/markdown/renderer.ts
@@ -298,7 +298,7 @@ async function parse({
 		blockquote(token) {
 			let content = this.parser?.parse(token.tokens) ?? '';
 			if (content.includes('[!LEGACY]')) {
-				content = `<details class="legacy"><summary>Legacy mode</summary>${content.replace('[!LEGACY]', '')}</details>`;
+				content = `<details class="legacy"><summary><label>Legacy mode <button class="raised"></button></label></summary>${content.replace('[!LEGACY]', '')}</details>`;
 			}
 			return `<blockquote>${content}</blockquote>`;
 		}
