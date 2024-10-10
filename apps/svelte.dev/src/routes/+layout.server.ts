0358ab968840e465e8f742533882a7daaebf1e3b
Rich Harris <rich.harris@vercel.com>
2024-10-10 12:01:01 -0400

    simplify

--- apps/svelte.dev/src/routes/+layout.server.ts
+++ apps/svelte.dev/src/routes/+layout.server.ts
@@ -2,8 +2,54 @@ import { docs as _docs, index } from '$lib/server/content';
 import { fetchBanner } from '@sveltejs/site-kit/components';
 import type { NavigationLink } from '@sveltejs/site-kit';
 
+const nav_links: NavigationLink[] = [
+	{
+		title: 'Docs',
+		prefix: 'docs',
+		pathname: '/docs',
+		sections: Object.values(_docs.topics)
+			.map((topic) => ({
+				title: topic.metadata.title,
+				path: '/' + topic.slug, // this will make the UI show a flyout menu for the docs nav entry
+				sections: topic.children.map((section) => ({
+					title: section.metadata.title,
+					sections: section.children.map((page) => ({
+						title: page.metadata.title,
+						path: '/' + page.slug
+					}))
+				}))
+			}))
+			.sort((a, b) => a.title.localeCompare(b.title)) // Svelte first
+	},
+	{
+		title: 'Tutorial',
+		prefix: 'tutorial',
+		pathname: '/tutorial',
+		sections: index.tutorial.children.map((topic) => ({
+			title: topic.metadata.title,
+			sections: topic.children.map((section) => ({
+				title: section.metadata.title,
+				sections: section.children.map((page) => ({
+					title: page.metadata.title,
+					path: '/tutorial/' + page.slug.split('/').pop()
+				}))
+			}))
+		}))
+	},
+	{
+		title: 'Playground',
+		prefix: 'playground',
+		pathname: '/playground'
+	},
+	{
+		title: 'Blog',
+		prefix: 'blog',
+		pathname: '/blog'
+	}
+];
+
 export const load = async ({ url, fetch }) => {
-	const [nav_links, banner] = await Promise.all([get_nav_list(), fetchBanner('svelte.dev', fetch)]);
+	const banner = await fetchBanner('svelte.dev', fetch);
 
 	return {
 		nav_title: get_nav_title(url),
@@ -30,56 +76,4 @@ function get_nav_title(url: URL) {
 	}
 
 	return '';
-}
-
-async function get_nav_list(): Promise<NavigationLink[]> {
-	const docs = Object.values(_docs.topics)
-		.map((topic) => ({
-			title: topic.metadata.title,
-			path: '/' + topic.slug, // this will make the UI show a flyout menu for the docs nav entry
-			sections: topic.children.map((section) => ({
-				title: section.metadata.title,
-				sections: section.children.map((page) => ({
-					title: page.metadata.title,
-					path: '/' + page.slug
-				}))
-			}))
-		}))
-		.sort((a, b) => a.title.localeCompare(b.title)); // Svelte first
-
-	const tutorial = index.tutorial.children.map((topic) => ({
-		title: topic.metadata.title,
-		sections: topic.children.map((section) => ({
-			title: section.metadata.title,
-			sections: section.children.map((page) => ({
-				title: page.metadata.title,
-				path: '/tutorial/' + page.slug.split('/').pop()
-			}))
-		}))
-	}));
-
-	return [
-		{
-			title: 'Docs',
-			prefix: 'docs',
-			pathname: '/docs',
-			sections: docs
-		},
-		{
-			title: 'Tutorial',
-			prefix: 'tutorial',
-			pathname: '/tutorial',
-			sections: tutorial
-		},
-		{
-			title: 'Playground',
-			prefix: 'playground',
-			pathname: '/playground'
-		},
-		{
-			title: 'Blog',
-			prefix: 'blog',
-			pathname: '/blog'
-		}
-	];
 }
