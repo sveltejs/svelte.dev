457a2f237b4df431884c050bbd5c0fb207ce25da
Rich Harris <rich.harris@vercel.com>
2024-10-09 23:53:00 -0400

    share some code

--- apps/svelte.dev/src/routes/(authed)/playground/[id]/UserMenu.svelte
+++ apps/svelte.dev/src/routes/(authed)/playground/[id]/UserMenu.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
-	import { Icon } from '@sveltejs/site-kit/components';
-	import { click_outside, focus_outside } from '@sveltejs/site-kit/actions';
+	import { Dropdown, Icon } from '@sveltejs/site-kit/components';
 	import { get_app_context } from '../../app-context';
 	import type { User } from '$lib/db/session';
 
@@ -12,56 +11,33 @@
 	let name = $derived(user.github_name ?? user.github_login);
 </script>
 
-<div
-	class="user"
-	use:focus_outside={() => (showMenu = false)}
-	use:click_outside={() => (showMenu = false)}
->
-	<button
-		onclick={() => (showMenu = !showMenu)}
-		aria-expanded={showMenu}
-		class="trigger"
-		aria-label={name}
-	>
+<Dropdown align="right">
+	<div class="user">
 		<span class="name">{name}</span>
 		<img alt="{name} avatar" src={user.github_avatar_url} />
 		<Icon size={18} name={showMenu ? 'chevron-up' : 'chevron-down'} />
-	</button>
+	</div>
 
-	{#if showMenu}
-		<div class="menu">
-			<a href="/apps">Your saved apps</a>
-			<button onclick={logout}>Log out</button>
-		</div>
-	{/if}
-</div>
+	{#snippet dropdown()}
+		<a href="/apps">Your saved apps</a>
+		<button onclick={logout}>Log out</button>
+	{/snippet}
+</Dropdown>
 
 <style>
 	.user {
 		position: relative;
-		display: inline-block;
-		padding: 0em 0 0 0.4rem;
-		z-index: 99;
-	}
-
-	.trigger {
 		display: flex;
 		align-items: center;
-		outline-offset: 2px;
-		transform: translateY(0.1rem);
-		--opacity: 0.7;
-	}
-
-	.trigger:hover,
-	.trigger:focus-visible,
-	.trigger[aria-expanded='true'] {
-		--opacity: 1;
+		padding: 0em 0 0 0.4rem;
+		height: calc(var(--app-controls-h) - 1.5rem);
+		z-index: 99;
 	}
 
 	.name {
 		display: none;
 		font-family: var(--sk-font-ui);
-		font-size: var(--sk-font-size-ui-small);
+		font-size: var(--sk-font-size-ui-medium);
 	}
 
 	.name {
@@ -81,41 +57,5 @@
 		border: 1px solid rgba(255, 255, 255, 0.3);
 		border-radius: var(--sk-border-radius);
 		transform: translateY(-0.1rem);
-	}
-
-	.menu {
-		position: absolute;
-		width: calc(100% + 1.6rem);
-		min-width: 10em;
-		top: 3rem;
-		right: -1.6rem;
-		background-color: var(--sk-back-2);
-		padding: 0.8rem 1.6rem;
-		z-index: 99;
-		text-align: left;
-		border-radius: 0.4rem;
-		display: flex;
-		flex-direction: column;
-	}
-
-	.menu button,
-	.menu a {
-		background-color: transparent;
-		font-family: var(--sk-font-body);
-		font-size: 1.6rem;
-		opacity: 0.7;
-		padding: 0.4rem 0;
-		text-decoration: none;
-		text-align: left;
-		border: none;
-		color: var(--sk-text-2);
-	}
-
-	.menu button:hover,
-	.menu button:focus-visible,
-	.menu a:hover,
-	.menu a:focus-visible {
-		opacity: 1;
-		color: inherit;
 	}
 </style>
