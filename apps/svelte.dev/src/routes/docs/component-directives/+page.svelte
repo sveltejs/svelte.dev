e8f59f9312452fd99f9480a1b1eb39919063ab7b
Ben McCann <322311+benmccann@users.noreply.github.com>
2024-10-24 21:04:40 -0700

    format

--- apps/svelte.dev/src/routes/docs/component-directives/+page.svelte
+++ apps/svelte.dev/src/routes/docs/component-directives/+page.svelte
@@ -2,7 +2,10 @@
 	import RemovedPage from '../RemovedPage.svelte';
 
 	const docs = new Map([
-		['on-eventname ', ['on:eventname', '/docs/svelte/v5-migration-guide#Event-changes-Component-events']],
+		[
+			'on-eventname ',
+			['on:eventname', '/docs/svelte/v5-migration-guide#Event-changes-Component-events']
+		],
 		['style-props', ['--style-props', '/docs/svelte/custom-properties']],
 		['bind-property ', ['bind:property', '/docs/svelte/bind#bind:property-for-components']],
 		['bind-this ', ['bind:this', '/docs/svelte/bind#bind:this']]
