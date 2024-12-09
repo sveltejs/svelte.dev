e446b9b49c7ccfb4f7b30ace227902f757a23624
Simon Holthausen <simon.holthausen@vercel.com>
2024-12-09 15:27:00 +0100

    work around Vercel's route limitation

--- apps/svelte.dev/src/routes/e/[code]/+page.server.ts
+++ apps/svelte.dev/src/routes/e/[code]/+page.server.ts
@@ -1,41 +1,38 @@
-import { index, remove_section } from '$lib/server/content';
+import { remove_section } from '$lib/server/content';
 import { error, redirect } from '@sveltejs/kit';
 
 // All links to warnings/errors from the Svelte compiler/runtime go through this page in order to have stable references
 // (i.e. we can move codes between warnings/errors/different pages or even adjust codes without breaking links).
-const reference = index['docs/svelte/reference'].children.filter(
-	(child) => child.slug.endsWith('-errors') || child.slug.endsWith('-warnings')
-);
 
-export const prerender = true;
+// Right now we can't prerender this because we would hit a "too many routes" error on Vercel,
+// for which we need to implement https://github.com/sveltejs/kit/issues/9032
+// const reference = index['docs/svelte/reference'].children.filter(
+// 	(child) => child.slug.endsWith('-errors') || child.slug.endsWith('-warnings')
+// );
+//
+// export const prerender = true;
+//
+// export function entries() {
+// 	return reference.flatMap((page) =>
+// 		[...page.body.matchAll(/(^|\n)### (\w+)/g)].map(([, , code]) => ({ code }))
+// 	);
+// }
 
-export function entries() {
-	return reference.flatMap((page) =>
-		[...page.body.matchAll(/(^|\n)### (\w+)/g)].map(([, , code]) => ({ code }))
+export async function load({ params, fetch }) {
+	const codes: Record<string, Record<string, string[]>> = await fetch('/e/codes.json').then((r) =>
+		r.json()
 	);
-}
 
-export function load({ params }) {
-	// Since codes are not top level section we gotta jump through some hoops to get the right hash
-	for (const page of reference) {
-		const idx = page.body.indexOf(`### ${params.code}`);
-		if (idx === -1) continue;
-
-		const slug_idx = page.body.lastIndexOf('\n## ', idx);
-
-		if (slug_idx > 0 || page.body.startsWith(`## `)) {
-			const title = page.body.slice(slug_idx + 4, page.body.indexOf('\n', slug_idx + 4)).trim();
-			const slug = page.sections.find((s) => s.title === title)?.slug;
-
-			if (!slug) {
-				throw new Error(
-					`Internal Error: Could not find previous title for code ${params.code} on ${page.slug}`
-				);
+	for (const url of Object.keys(codes)) {
+		const page = codes[url];
+		for (const [h2, h3] of Object.entries(page)) {
+			if (h3.includes(params.code)) {
+				if (h2) {
+					redirect(307, `/${remove_section(url)}#${h2}-${params.code}`);
+				} else {
+					redirect(307, `/${remove_section(url)}#${params.code}`);
+				}
 			}
-
-			redirect(307, `/${remove_section(page.slug)}#${slug}-${params.code}`);
-		} else {
-			redirect(307, `/${remove_section(page.slug)}#${params.code}`);
 		}
 	}
 
