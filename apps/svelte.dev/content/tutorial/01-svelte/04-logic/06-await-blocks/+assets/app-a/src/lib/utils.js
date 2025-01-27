208687d5d9f50e923c30ab75a4abc04f4745d445
Rich Werden <r-i-c-h@users.noreply.github.com>
2025-01-27 17:11:07 -0500

    triv: Update utils.js fix spelling
    triv: Spelling 'fulfil' -> 'fulfill'

--- apps/svelte.dev/content/tutorial/01-svelte/04-logic/06-await-blocks/+assets/app-a/src/lib/utils.js
+++ apps/svelte.dev/content/tutorial/01-svelte/04-logic/06-await-blocks/+assets/app-a/src/lib/utils.js
@@ -1,7 +1,7 @@
 export async function roll() {
 	// Fetch a random number from 1 to 6
 	// (with a delay, so that we can see it)
-	return new Promise((fulfil, reject) => {
+	return new Promise((fulfill, reject) => {
 		setTimeout(() => {
 			// simulate a flaky network
 			if (Math.random() < 0.3) {
@@ -9,7 +9,7 @@ export async function roll() {
 				return;
 			}
 
-			fulfil(Math.ceil(Math.random() * 6));
+			fulfill(Math.ceil(Math.random() * 6));
 		}, 1000);
 	});
 }
