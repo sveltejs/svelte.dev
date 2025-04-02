a7cfde4b9995b6cb419c71a6114c6d7430e5ef5a
Rich Harris <rich.harris@vercel.com>
2025-04-02 18:27:10 -0400

    remove

--- packages/repl/src/lib/public.d.ts
+++ packages/repl/src/lib/public.d.ts
@@ -3,8 +3,8 @@ import type { OutputChunk, RollupError } from '@rollup/browser';
 export interface BundleResult {
 	uid: number;
 	error: (RollupError & CompileError) | null;
-	client: OutputChunk | null; // TODO
-	server: OutputChunk | null; // TODO
+	client: OutputChunk | null;
+	server: OutputChunk | null;
 	tailwind: string | null;
 	imports: string[];
 }
