b0c8803b5a77ed541c63931f57ec3515c44eb3d4
Rich Harris <rich.harris@vercel.com>
2025-04-15 11:07:24 -0400

    tweak markup

--- apps/svelte.dev/src/routes/packages/PackageCard.svelte
+++ apps/svelte.dev/src/routes/packages/PackageCard.svelte
@@ -13,23 +13,13 @@
 	<span class="header">
 		<h3 class={[(pkg.outdated || pkg.deprecated) && 'faded']}>
 			{pkg.name}
-		</h3>
-		<span class="status">
+
 			{#if pkg.typescript}
-				<span style="translate: 0 2px;">
-					{#if pkg.typescript}
-						<svg xmlns="http://www.w3.org/2000/svg" width="1.3em" height="1.3em" viewBox="0 0 24 24"
-							><!-- Icon from Simple Icons by Simple Icons Collaborators - https://github.com/simple-icons/simple-icons/blob/develop/LICENSE.md --><path
-								fill="currentColor"
-								d="M1.125 0C.502 0 0 .502 0 1.125v21.75C0 23.498.502 24 1.125 24h21.75c.623 0 1.125-.502 1.125-1.125V1.125C24 .502 23.498 0 22.875 0zm17.363 9.75q.918 0 1.627.111a6.4 6.4 0 0 1 1.306.34v2.458a4 4 0 0 0-.643-.361a5 5 0 0 0-.717-.26a5.5 5.5 0 0 0-1.426-.2q-.45 0-.819.086a2.1 2.1 0 0 0-.623.242q-.254.156-.393.374a.9.9 0 0 0-.14.49q0 .294.156.529q.156.234.443.444c.287.21.423.276.696.41q.41.203.926.416q.705.296 1.266.628q.561.333.963.753q.402.418.614.957q.213.538.214 1.253q0 .986-.373 1.656a3 3 0 0 1-1.012 1.085a4.4 4.4 0 0 1-1.487.596q-.85.18-1.79.18a10 10 0 0 1-1.84-.164a5.5 5.5 0 0 1-1.512-.493v-2.63a5.03 5.03 0 0 0 3.237 1.2q.5 0 .872-.09q.373-.09.623-.25q.249-.162.373-.38a1.02 1.02 0 0 0-.074-1.089a2.1 2.1 0 0 0-.537-.5a5.6 5.6 0 0 0-.807-.444a28 28 0 0 0-1.007-.436q-1.377-.575-2.053-1.405t-.676-2.005q0-.92.369-1.582q.368-.662 1.004-1.089a4.5 4.5 0 0 1 1.47-.629a7.5 7.5 0 0 1 1.77-.201m-15.113.188h9.563v2.166H9.506v9.646H6.789v-9.646H3.375z"
-							/></svg
-						>
-					{/if}
-				</span>
+				<span data-icon="typescript"></span>
 			{/if}
+		</h3>
 
-			<span style="flex: 1 1 auto"></span>
-
+		<span class="status">
 			{#if pkg.runes}
 				<span class="pill runes">runes</span>
 			{/if}
@@ -104,13 +94,12 @@
 		display: flex;
 		align-items: center;
 		margin-bottom: 1rem;
+		justify-content: space-between;
 	}
 
 	h3 {
 		display: inline-block;
 		font: var(--sk-font-ui-medium);
-
-		/* flex: 1 1 auto; */
 
 		&.faded {
 			color: var(--sk-fg-3);
@@ -143,9 +132,42 @@
 			color: inherit;
 		}
 
-		.status {
-			flex: 1 1 auto;
+		[data-icon] {
+			position: relative;
+			display: inline-flex;
+			width: 1.4rem;
+			height: 1.4rem;
+			background: currentColor;
+			mask: no-repeat 50% 50%;
+			mask-size: contain;
 
+			&[data-icon='star'] {
+				mask-image: url(icons/star);
+			}
+
+			&[data-icon='typescript'] {
+				mask-image: url(icons/typescript);
+			}
+
+			&[data-icon='download'] {
+				mask-image: url(icons/download);
+			}
+
+			&[data-icon='github'] {
+				mask-image: url(icons/github);
+			}
+
+			&[data-icon='npm'] {
+				mask-image: url(icons/npm);
+			}
+
+			&[data-icon='external-link'] {
+				mask-image: url(icons/external-link);
+				mask-size: 1.7rem;
+			}
+		}
+
+		.status {
 			display: flex;
 			align-items: center;
 			gap: 0.5rem;
@@ -212,36 +234,6 @@
 				display: flex;
 				gap: 0.4rem;
 				align-items: center;
-			}
-
-			[data-icon] {
-				display: flex;
-				width: 1.4rem;
-				height: 1.4rem;
-				background: currentColor;
-				mask: no-repeat 50% 50%;
-				mask-size: contain;
-			}
-
-			[data-icon='star'] {
-				mask-image: url(icons/star);
-			}
-
-			[data-icon='download'] {
-				mask-image: url(icons/download);
-			}
-
-			[data-icon='github'] {
-				mask-image: url(icons/github);
-			}
-
-			[data-icon='npm'] {
-				mask-image: url(icons/npm);
-			}
-
-			[data-icon='external-link'] {
-				mask-image: url(icons/external-link);
-				mask-size: 1.7rem;
 			}
 		}
 
