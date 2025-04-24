bddf58f2e241ec04e1e4f18e92c88413747bc067
Ben McCann <322311+benmccann@users.noreply.github.com>
2025-04-24 08:26:07 -0700

    content placeholders

--- apps/svelte.dev/src/routes/packages/PackageDetails.svelte
+++ apps/svelte.dev/src/routes/packages/PackageDetails.svelte
@@ -10,6 +10,8 @@
 	};
 
 	const { pkg }: Props = $props();
+
+	const encoded_pkg = $derived(encodeURIComponent(pkg.name));
 </script>
 
 <a href="/packages" class="raised"> &nbsp; Back &nbsp;</a>
@@ -24,58 +26,72 @@
 
 <p>{pkg.description}</p>
 
+<div style="display: flex; gap: 0.75rem">
+	<a
+		href="https://npmjs.org/package/{pkg.name}"
+		target="_blank"
+		rel="noreferrer"
+		aria-label="View on npm">npm</a
+	>
+
+	{#if pkg.repo_url}
+		<a href={pkg.repo_url} target="_blank" rel="noreferrer" aria-label="View on GitHub">github</a>
+	{/if}
+
+	{#if pkg.homepage}
+		<a href={pkg.homepage} target="_blank" rel="noreferrer" aria-label="View project homepage"
+			>homepage</a
+		>
+	{/if}
+</div>
+
+<br /><br />
+
+<h2>Package Popularity</h2>
+
 <p class="stats">
 	{#if pkg.downloads}
 		<span title="{pkg.downloads} downloads">
 			<span data-icon="download"></span>
-			{format_number(+pkg.downloads)}
+			{format_number(+pkg.downloads)} (<a href="https://www.npmcharts.com/compare/{pkg.name}">+99% YoY</a>)
 		</span>
 	{/if}
 
 	{#if pkg.github_stars}
 		<span title="{pkg.github_stars} Github Stars">
 			<span data-icon="star"></span>
-			{format_number(pkg.github_stars)}
+			{format_number(pkg.github_stars)} (<a href="https://www.star-history.com/#{pkg.repo_url.substring('https://github.com/'.length)}&Date">+99% YoY</a>)
 		</span>
-	{/if}
-
-	{#if pkg.typescript}
-		<span data-icon="typescript"></span>
 	{/if}
 
 	{#if pkg.official}
 		<span data-icon="svelte"></span>
 	{/if}
-
-	<span style="display: flex; gap: 0.75rem">
-		<a
-			href="https://npmjs.org/package/{pkg.name}"
-			target="_blank"
-			rel="noreferrer"
-			aria-label="View on npm">npm</a
-		>
-
-		{#if pkg.repo_url}
-			<a href={pkg.repo_url} target="_blank" rel="noreferrer" aria-label="View on GitHub">github</a>
-		{/if}
-
-		{#if pkg.homepage}
-			<a href={pkg.homepage} target="_blank" rel="noreferrer" aria-label="View project homepage"
-				>homepage</a
-			>
-		{/if}
-	</span>
 </p>
 
-<code>pnpm add {pkg.name}</code>
+<br />
 
-<br /><br /><br />
+<!-- <Chart history={pkg.downloads_history} /> -->
 
-<Chart history={pkg.downloads_history} />
+<h2>Package Health</h2>
 
-<div style="width: 100%; overflow-x: auto">
+<p>
+Type definitions: {pkg.typescript ? 'included' : 'missing'}<br />
+Svelte 5 rune syntax: adopted / legacy<br />
+Publint errors: 0
+</p>
+
+<h2>Package Weight</h2>
+<p><a href="https://npmgraph.js.org/?q={encoded_pkg}" title="999 transitive dependencies">999 dependencies</a></p>
+<p>{format_bytes(pkg.dependency_tree.packages.reduce((acc, pkg) => acc + (pkg.size ?? 0), 0))} in server's node_modules</p>
+<p>
+	<a href="https://bundlejs.com/?q={encoded_pkg}"><img src="https://deno.bundlejs.com/?q={encoded_pkg}&badge=minified&badge-style=for-the-badge"></a>
+	<a href="https://bundlejs.com/?q={encoded_pkg}"><img src="https://deno.bundlejs.com/?q={encoded_pkg}&badge&badge-style=for-the-badge"></a>
+</p>
+
+<!-- <div style="width: 100%; overflow-x: auto">
 	<img src="/packages/{pkg.name}/graph/dependency.svg" alt="Dependency graph" />
-</div>
+</div> -->
 
 <!-- <DependencyGraph graph={pkg.dependency_tree} /> -->
 
@@ -88,10 +104,9 @@
 	</ul>
 {/if} -->
 
-<br /><br />
+<h2>README</h2>
 
-<h2>Install Size</h2>
-<p>{format_bytes(pkg.dependency_tree.packages.reduce((acc, pkg) => acc + (pkg.size ?? 0), 0))}</p>
+<p>goes here...</p>
 
 <style>
 	*:not(h1, h2) {
