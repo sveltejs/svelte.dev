3c0b9ceeb539337421c6361338912d8c64a19b19
Puru Vijay <awesomepuruvj@gmail.com>
2025-05-05 12:45:31 +0530

    Prerender package pages

--- apps/svelte.dev/src/routes/packages/[...pkg]/+page.server.ts
+++ apps/svelte.dev/src/routes/packages/[...pkg]/+page.server.ts
@@ -2,7 +2,7 @@ import { registry } from '$lib/server/content';
 import { render_content } from '$lib/server/renderer.js';
 import { error } from '@sveltejs/kit';
 
-export const prerender = false;
+export const prerender = true;
 
 export async function load({ params }) {
 	const pkg = registry.find((pkg) => pkg.name === params.pkg);
