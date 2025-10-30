7ad7583576fe59308e38807d2abbf1722519b1a9
Rich Harris <rich.harris@vercel.com>
2025-10-30 23:15:54 +0100

    uncomment speed insights in prod

--- apps/svelte.dev/src/routes/+layout.svelte
+++ apps/svelte.dev/src/routes/+layout.svelte
@@ -10,8 +10,10 @@
 	import { beforeNavigate } from '$app/navigation';
 	import { get_banner, get_nav_links } from './data.remote.js';
 
-	// injectSpeedInsights();
-	// inject({ mode: dev ? 'development' : 'production' });
+	if (!dev) {
+		injectSpeedInsights();
+		inject({ mode: 'production' });
+	}
 
 	// Make all navigations between SvelteKit-tutorial and non-SvelteKit-tutorial pages (and vice versa)
 	// a full page navigation to ensure webcontainers get the correct origin restriction headers while
