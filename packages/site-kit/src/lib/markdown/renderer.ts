34ee5484788f43eb7f8e4d39eb5d1fae3180fce5
Rich Harris <rich.harris@vercel.com>
2026-02-24 05:37:43 -0500

    Fix highlighting (#1812)
    * balance highlighting spans
    
    * another fix
    
    * fix
    
    * robustify highlighting

--- packages/site-kit/src/lib/markdown/renderer.ts
+++ packages/site-kit/src/lib/markdown/renderer.ts
@@ -859,15 +859,40 @@ const delimiter_substitutes = {
 const delimiter_patterns = Object.fromEntries(
 	Object.entries(delimiter_substitutes).map(([key, substitute]) => [
 		key,
-		new RegExp(`${substitute}([^ ][^]+?)${substitute}`, 'g')
+		new RegExp(`${substitute}([^ ]|[^ ][^]+?[^ ])${substitute}`, 'g')
 	])
 );
 
-function highlight_spans(content: string, classname: string) {
-	return content
-		.split('\n')
-		.map((line) => `<span class="${classname}">${line}</span>`)
-		.join('\n');
+function highlight_all_spans(html: string, pattern: RegExp, classname: string) {
+	const open = `<span class="${classname}">`;
+
+	return html.replace(pattern, (_, content, index) => {
+		let a = content.indexOf('<span');
+		let b = content.indexOf('</span');
+		let c = content.lastIndexOf('<span');
+		let d = content.lastIndexOf('</span');
+
+		let adjusted: string = content;
+
+		if (b !== -1 && (a === -1 || b < a)) {
+			// starts inside a <span>
+			const tag_start = html.lastIndexOf('<span', index);
+			const tag = html.slice(tag_start, html.indexOf('>', tag_start) + 1);
+			adjusted = `</span>${open}${tag}${adjusted}`;
+		} else {
+			adjusted = `${open}${adjusted}`;
+		}
+
+		if (c !== -1 && (d === -1 || c > d)) {
+			// ends inside a <span>
+			const tag = content.slice(c, content.indexOf('>', c) + 1);
+			adjusted = `${adjusted}</span></span>${tag}`;
+		} else {
+			adjusted = `${adjusted}</span>`;
+		}
+
+		return adjusted.replace(/\n/g, `</span>\n${open}`);
+	});
 }
 
 async function syntax_highlight({
@@ -1034,16 +1059,9 @@ async function syntax_highlight({
 		// remove tabindex
 		.replace(' tabindex="0"', '');
 
-	html = html
-		.replace(delimiter_patterns['---'], (_, content) => {
-			return highlight_spans(content, 'highlight remove');
-		})
-		.replace(delimiter_patterns['+++'], (_, content) => {
-			return highlight_spans(content, 'highlight add');
-		})
-		.replace(delimiter_patterns[':::'], (_, content) => {
-			return highlight_spans(content, 'highlight');
-		});
+	html = highlight_all_spans(html, delimiter_patterns['---'], 'highlight remove');
+	html = highlight_all_spans(html, delimiter_patterns['+++'], 'highlight add');
+	html = highlight_all_spans(html, delimiter_patterns[':::'], 'highlight');
 
 	return indent_multiline_comments(html)
 		.replace(/\/\*…\*\//g, '…')
