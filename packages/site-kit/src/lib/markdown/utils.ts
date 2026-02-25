a2abe0cbaea90099b6bf12783e4b5e4582487767
Tee Ming <chewteeming01@gmail.com>
2026-02-25 23:16:08 +0800

    fix: correctly slugify special section titles (#1815)
    * fix links
    
    * upgrade marked
    
    * format
    
    * these need to be upgraded too
    
    * more fixes
    
    * format
    
    * need to remove after decode
    
    * fix multiple code tags not being stripped
    
    * add private frontmatter attribute
    
    * comment

--- packages/site-kit/src/lib/markdown/utils.ts
+++ packages/site-kit/src/lib/markdown/utils.ts
@@ -35,28 +35,54 @@ export function is_in_code_block(body: string, index: number) {
  * Strip styling/links etc from markdown
  */
 export function clean(markdown: string) {
-	return markdown
-		.replace(/(?:^|b)\*\*(.+?)\*\*(?:\b|$)/g, '$1') // bold
-		.replace(/(?:^|b)_(.+?)_(?:\b|$)/g, '$1') // Italics
-		.replace(/(?:^|b)\*(.+?)\*(?:\b|$)/g, '$1') // Italics
-		.replace(/(?:^|b)`(.+?)`(?:\b|$)/g, '$1') // Inline code
-		.replace(/(?:^|b)~~(.+?)~~(?:\b|$)/g, '$1') // Strikethrough
-		.replace(/\[(.+?)\]\(.+?\)/g, '$1') // Link
-		.replace(/\n/g, ' ') // New line
-		.replace(/ {2,}/g, ' ')
-		.trim();
+	return (
+		markdown
+			.replace(/(?:^|b)\*\*(.+?)\*\*(?:\b|$)/g, '$1') // bold
+			.replace(/(?:^|b)_(.+?)_(?:\b|$)/g, '$1') // Italics
+			.replace(/(?:^|b)\*(.+?)\*(?:\b|$)/g, '$1') // Italics
+			// italic markdown notation such as "bind:_property_ for components"
+			// should be stripped without affecting compiler error titles such as "animation_missing_key"
+			.replace(/:_(.*)_ /g, ':$1 ')
+			.replace(/(?:^|b)`(.+?)`(?:\b|$)/g, '$1') // Inline code
+			.replace(/(?:^|b)~~(.+?)~~(?:\b|$)/g, '$1') // Strikethrough
+			.replace(/\[(.+?)\]\(.+?\)/g, '$1') // Link
+			.replace(/\n/g, ' ') // New line
+			.replace(/ {2,}/g, ' ')
+			.trim()
+	);
+}
+
+export function decode_html_entities(text: string): string {
+	return text
+		.replace(/&lt;/g, '<')
+		.replace(/&gt;/g, '>')
+		.replace(/&amp;/g, '&')
+		.replace(/&quot;/g, '"')
+		.replace(/&#x([0-9a-fA-F]+);/g, (_, hex) => {
+			return String.fromCharCode(parseInt(hex, 16));
+		})
+		.replace(/&#(\d+);/g, (_, dec) => {
+			return String.fromCharCode(parseInt(dec, 10));
+		});
 }
 
 export const slugify = (str: string) => {
-	return clean(str)
-		.replace(/(’|&rsquo;)/g, "'")
-		.replace(/&.+?;/g, '')
-		.replace(/<\/?.+?>/g, '')
-		.replace(/\.\.\./g, '')
-		.replace(/[^a-zA-Z0-9-$(.):'_]/g, '-')
-		.replace(/-{2,}/g, '-')
-		.replace(/^-/, '')
-		.replace(/-$/, '');
+	return (
+		decode_html_entities(clean(str).replace(/(’|&rsquo;)/g, "'"))
+			// removes <code>...</code> or <em>...</em> etc, but leaves the contents intact
+			.replace(/<([a-z\-]+)>(.*?)<\/\1>/g, '$2')
+			// <audio> should be converted to audio
+			// <details bind:open> should be converted to details-bind-open
+			// <script module> should be converted to script-module
+			// <script lang="ts"> should be converted to script-lang-ts
+
+			.replace(/[<>]/g, '')
+			.replace(/\.\.\./g, '')
+			.replace(/[^a-zA-Z0-9-$(.):'_]/g, '-')
+			.replace(/-{2,}/g, '-')
+			.replace(/^-/, '')
+			.replace(/-$/, '')
+	);
 };
 
 export function smart_quotes(
