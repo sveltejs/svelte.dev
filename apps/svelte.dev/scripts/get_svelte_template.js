3866e6258216e0eaecfe2981219d8dc76b93ad94
Conduitry <git@chor.date>
2026-04-17 01:01:01 -0400

    drive-by typo fixes

--- apps/svelte.dev/scripts/get_svelte_template.js
+++ apps/svelte.dev/scripts/get_svelte_template.js
@@ -4,7 +4,7 @@ import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 import { create } from 'sv';
 
-// This download the currente Vite template from Github, adjusts it to our needs, and saves it to static/svelte-template.json
+// This downloads the current Vite template from GitHub, adjusts it to our needs, and saves it to static/svelte-template.json
 // This is used by the Svelte REPL as part of the "download project" feature
 
 const force = process.env.FORCE_UPDATE === 'true';
