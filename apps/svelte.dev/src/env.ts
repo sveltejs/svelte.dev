8ead665eb99c0baf593b1e19cf95682d5b29d9cc
Rich Harris <rich.harris@vercel.com>
2026-07-14 07:21:19 -0400

    revert `availability` change
    Removed unnecessary comments and updated static property.

--- apps/svelte.dev/src/env.ts
+++ apps/svelte.dev/src/env.ts
@@ -19,8 +19,7 @@ export const variables = defineEnvVars({
 		schema: v.optional(v.string(), '')
 	},
 	PRERENDER: {
-		static: true as any, // TODO: remove this when we stop deploying previews for Kit 2
-		availability: 'inline'
+		static: true
 	}
 });
 
