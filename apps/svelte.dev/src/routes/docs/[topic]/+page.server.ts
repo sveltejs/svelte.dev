f5da3c3d55d53305e11633f687b221a71728a102
Rich Harris <rich.harris@vercel.com>
2026-08-10 10:11:54 -0400

    fix: 404 on bad docs link (#2151)

--- apps/svelte.dev/src/routes/docs/[topic]/+page.server.ts
+++ apps/svelte.dev/src/routes/docs/[topic]/+page.server.ts
@@ -1,8 +1,8 @@
 import { docs } from '#lib/server/content';
-import { redirect } from '@sveltejs/kit';
+import { error, redirect } from '@sveltejs/kit';
 
 export async function load({ params }) {
-	const document = docs.topics[`docs/${params.topic}`];
+	const document = docs.topics[`docs/${params.topic}`] ?? error(404);
 
 	redirect(307, `/${document.children[0].children[0].slug}`);
 }
