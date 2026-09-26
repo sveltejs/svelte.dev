9f0d6ae868c359dea87a83d3bb25a4b2c5277cdc
Rich Harris <rich.harris@vercel.com>
2026-09-26 09:49:08 -0400

    ??

--- apps/svelte.dev/src/routes/(authed)/playground/[...id]/+page.server.ts
+++ apps/svelte.dev/src/routes/(authed)/playground/[...id]/+page.server.ts
@@ -2,6 +2,7 @@ import { error } from '@sveltejs/kit';
 import type { Examples } from '../api/examples/all.json/+server.js';
 
 export async function load({ fetch, params }) {
+	console.log('>>> here');
 	const examples_res = fetch('/playground/api/examples/all.json').then((r) => r.json());
 	console.log('examples', examples_res);
 	const res = await fetch(`/playground/api/${params.id}.json`);
